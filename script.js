e8d0b0a10ce02625ff5f9e9a6a7c2f88ea78b789
Uddhav <uddhavsaikia@gmail.com>
2024-06-27 01:55:07 +0530

    added id to container

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const size=10;
-const container=document.querySelector("div");
+const container=document.getElementById("#kela");
 container.style.display="flex";
 container.style.flexWrap="wrap";
 
 function createGrid(size){
-    for(let x;x<size;x++){
+    for(let x; x<size ; x++){
         const div=document.createElement("div");
         div.style.width=100/size+'%';
         container.appendChild(div);
     }
 }
+
+createGrid(size);
+
 
 
