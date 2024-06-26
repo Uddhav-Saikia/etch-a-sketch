e52d3be4f885346e19846f52e7a30c5a3c5a9efa
Uddhav <uddhavsaikia@gmail.com>
2024-06-26 16:32:47 +0530

    func to add the div

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-const container=queryselector("div");
+const container=document.queryselector("div");
+const dimension=16
+
+function createTable(){
+    for (let x = 0; x < 16; x++) {
+        let row=document.createElement("div");
+    
+        for (let y = 0; x < 16; y++) {
+            row.appendChild(document.createElement("div"));
+        }
+        container.appendChild("row");
+    }
+}
+
 
