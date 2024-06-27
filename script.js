435bc1a3651420b02419fec911d4451808b7d7cf
Uddhav <uddhavsaikia@gmail.com>
2024-06-27 10:54:01 +0530

    modularity

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
 const size=10;
 const black="black";
-const container=document.querySelector("div");
+
 const body=document.querySelector("body");
-body.style.display="flex";
-body.style.justifyContent="center";
-body.style.alignItems="center";
 
+const ht=document.createElement("h1");
+ht.innerText="Hey";
+body.appendChild(ht);
 
+const container=document.createElement("div");
+body.appendChild(container);
 container.style.display="flex";
 container.style.flexWrap="wrap";
 container.style.width="80%";
 container.style.border="1px solid black";
 container.style.height="600px";
 container.style.width="600px";
+container.style.marginLeft="auto";
+container.style.marginRight="auto";
 
 
-function createGrid(size,colour){
-    
-    for(let x=0; x<size*size; x++){
-        const div=document.createElement('div');
-        div.classList.add("item");
-        div.style.width=100/size+'%';
-        div.style.height=100/size+'%';
-        container.appendChild(div);
-        div.style.opacity=0;
-        div.style.backgroundColor=colour;
 
-        div.addEventListener("mouseenter",
-            ()=>{
-                if(div.style.opacity<1){
-                    div.style.opacity=parseFloat(div.style.opacity)+0.15;
-                }
-        }
-        )
-    }
-}
+
 
 function getRandomColor(){
     let letter="0123456789ABCDEF";
@@ -46,9 +32,51 @@ function getRandomColor(){
 }
 
 
+function createGridBlack(size){
+    
+    for(let x=0; x<size*size; x++){
+        const div=document.createElement('div');
+        div.classList.add("item");
+        div.style.width=100/size+'%';
+        div.style.height=100/size+'%';
+        container.appendChild(div);
+        div.style.opacity=0;
+        div.style.backgroundColor="black";
+
+        div.addEventListener("mouseenter",
+            ()=>{
+                if(div.style.opacity<1){
+                    div.style.opacity=parseFloat(div.style.opacity)+0.15;
+                }
+        }
+        )
+    }
+}
 
 
-createGrid(size,getRandomColor);
+function createGridRandom(size){
+    
+    for(let x=0; x<size*size; x++){
+        const div=document.createElement('div');
+        div.classList.add("item");
+        div.style.width=100/size+'%';
+        div.style.height=100/size+'%';
+        container.appendChild(div);
+        div.style.opacity=0;
+        div.style.backgroundColor=getRandomColor();
+
+        div.addEventListener("mouseenter",
+            ()=>{
+                if(div.style.opacity<1){
+                    div.style.opacity=parseFloat(div.style.opacity)+0.15;
+                }
+        }
+        )
+    }
+}
+
+
+createGridRandom(size);
 
 
 
