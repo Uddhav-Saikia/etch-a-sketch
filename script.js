f29f2663844b88869e375b7594ab17131a52da41
Uddhav <uddhavsaikia@gmail.com>
2024-06-27 09:45:32 +0530

    grid container centered on screen

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 const size=10;
-const container=document.getElementById("kela");
+const container=document.querySelector("div");
+const body=document.querySelector("body");
+body.style.display="flex";
+body.style.justifyContent="center";
+body.style.alignItems="center";
+
 container.style.display="flex";
 container.style.flexWrap="wrap";
+container.style.width="80%";
+container.style.border="1px solid black";
+container.style.height="600px";
+container.style.width="600px";
+
+
+
 
 function createGrid(size){
-    for(let x; x<size ; x++){
-        const div=document.createElement("div");
+    const div=document.createElement('div');
+    for(let x=0; x<size*size; x++){
+        div.classList.add("item");
         div.style.width=100/size+'%';
+        div.style.height=100/size+'%';
         container.appendChild(div);
     }
+    // div.addEventListener("mouseenter",
+    //     div.style.backgroundColor+=
+    // )
 }
 
-createGrid(size);
+createGrid(10);
 
 
 
