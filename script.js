cf4583968fae45eb820c0c78b549086907aa9b29
Uddhav <uddhavsaikia@gmail.com>
2024-06-27 10:06:43 +0530

    added random colour...but not working

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ body.style.display="flex";
 body.style.justifyContent="center";
 body.style.alignItems="center";
 
+
 container.style.display="flex";
 container.style.flexWrap="wrap";
 container.style.width="80%";
@@ -13,20 +14,38 @@ container.style.height="600px";
 container.style.width="600px";
 
 
-
-
 function createGrid(size){
-    const div=document.createElement('div');
+    
     for(let x=0; x<size*size; x++){
+        const div=document.createElement('div');
         div.classList.add("item");
         div.style.width=100/size+'%';
         div.style.height=100/size+'%';
         container.appendChild(div);
+        div.style.opacity=0;
+
+        div.addEventListener("mouseenter",
+            ()=>{
+                div.style.backgroundColor=randomColor;
+                if(div.style.opacity<1){
+                    div.style.opacity=parseFloat(div.style.opacity)+0.15;
+                }
+        }
+        )
     }
-    // div.addEventListener("mouseenter",
-    //     div.style.backgroundColor+=
-    // )
 }
+
+function randomColor(){
+    r=Math.floor(Math.random()*255);
+    g=Math.floor(Math.random()*255);
+    b=Math.floor(Math.random()*255);
+ 
+    const colour=toString(rgb(r,g,b));
+    return colour;
+}
+
+
+
 
 createGrid(10);
 
